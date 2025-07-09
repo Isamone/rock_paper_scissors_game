11d387f2b557000d7633b3f6e9dbc3d8599b947e
Isamone <idamcida@yahoo.com>
2025-07-09 16:48:43 +0100

    Add files via upload

--- paper_scissors.py
+++ paper_scissors.py
@@ -4,21 +4,21 @@ import random
 st.title("✊ Rock, 🖐 Paper, ✌️ Scissors Game")
 
 options = ["Rock", "Paper", "Scissors"]
-player_choice = st.selectbox("Choose your move:", options)
+Saddiqahs_choice = st.selectbox("Choose your move:", options)
 
 if st.button("Play"):
     AI_ISMAIL_choice = random.choice(options)
 
-    st.write(f"🧍 You chose: **{player_choice}**")
-    st.write(f"🤖 AI_ISMAIL chose: **{AI_ISMAIL_choice}**")
+    st.write(f"🧍 You chose: **{Saddiqahs_choice}**")
+    st.write(f"🤖 AI ISMAIL chose: **{AI_ISMAIL_choice}**")
 
-    if player_choice == AI_ISMAIL_choice:
+    if Saddiqahs_choice == AI_ISMAIL_choice:
         st.info("It's a tie!")
     elif (
-        (player_choice == "Rock" and AI_ISMAIL_choice == "Scissors") or
-        (player_choice == "Scissors" and AI_ISMAIL_choice == "Paper") or
-        (player_choice == "Paper" and AI_ISMAIL_choice == "Rock")
+        (Saddiqahs_choice == "Rock" and AI_ISMAIL_choice == "Scissors") or
+        (Saddiqahs_choice == "Scissors" and AI_ISMAIL_choice == "Paper") or
+        (Saddiqahs_choice == "Paper" and AI_ISMAIL_choice == "Rock")
     ):
-        st.success("You win! 🎉")
+        st.success("Saddiqah wins! 🎉")
     else:
         st.error("AI_ISMAIL wins! 😢")
